effdcff03e16204f5057efb54b514fef92ffdd8d
David T <7316224-davidubik@users.noreply.gitlab.com>
2021-07-05 14:34:15 +0200

    Mise à jour du code.

--- js/front.js
+++ js/front.js
@@ -1,50 +1,33 @@
 /* JS CTREATED BY CKWS */
 
-// var header;
-// //var headerId;
+var header;
+var headerId;
 
-// document.addEventListener('DOMContentLoaded', function(){
-//   header = document.querySelector('.header');
-//   //headerId = document.querySelector('#header');  
-// });
+document.addEventListener('DOMContentLoaded', function(){
+  header = document.querySelector('.header');
+  headerId = document.querySelector('#header');  
   
-// function scrollingTo(){
-// 	window.scroll({
-// 	  top: window.innerHeight,
-// 	  behavior: 'smooth'
-// 	});
-// 	//headerId.classList.remove("header-active");
-// }
-
-// function wheelHandler(){
-// 	//if (!header.classList.contains('header-active')) {
-// 	//	return false
-// 	//}
-// 	scrollingTo();
-// }
-
-
-//SCROOL TO
-
-const header = document.querySelector('.header');
-const headerId = document.querySelector('#header');
-const imgMaximmo = document.querySelector('.maximmo');
-const rect = header.getBoundingClientRect();
-
-scrollingTo = () =>{
-	window.scrollTo(0,rect.bottom);
-	behavior: 'smooth';
-	console.log("Scroll : "+ window.scrollX + "," + window.scrollY);
-	headerId.classList.remove("header-active");
-  }
-
-  header.addEventListener('wheel', ()=>{
-	  wheelHandler();
-  })
-
-  function wheelHandler(){
+	window.addEventListener('wheel', (evt)=>{
+		evt.preventDefault();
+		evt.stopPropagation();
+        wheelHandler();
+    })
+  
+});
+  
+function scrollingTo(){
 	if (!header.classList.contains('header-active')) {
-	  return false
+		return false
 	}
+	window.scroll({
+	  top: window.innerHeight,
+	  behavior: 'smooth'
+	});
+	setTimeout(function(){
+		headerId.classList.remove("header-active");
+	},2000);
+}
+
+function wheelHandler(){
 	scrollingTo();
-  }
+}
